6b590625b522e5c6af2dc75fb4f6a4eb5eee1a4b
francgrasso <francgrasso@gmail.com>
2020-06-26 13:05:35 +0200

    fix wrong conditions for sidebar display

--- sidebar.php
+++ sidebar.php
@@ -14,20 +14,11 @@ if ( ! yith_proteo_get_sidebar_position( 'sidebar-show' ) ) {
 // WooCommerce shop page support.
 if ( function_exists( 'wc' ) && is_shop() && get_post_meta( get_option( 'woocommerce_shop_page_id' ), 'sidebar_position', true ) == 'no-sidebar' ) {
 	return;
-}
-
-// WooCommerce category page support.
-if ( function_exists( 'wc' ) && is_product_category() && get_theme_mod( 'yith_proteo_product_category_page_sidebar_position', 'no-sidebar' ) == 'no-sidebar' ) {
+} elseif ( function_exists( 'wc' ) && is_product_category() && ! is_product_taxonomy() && get_theme_mod( 'yith_proteo_product_category_page_sidebar_position', 'no-sidebar' ) == 'no-sidebar' ) {
 	return;
-}
-
-// WooCommerce tag page support.
-if ( function_exists( 'wc' ) && is_product_tag() && get_theme_mod( 'yith_proteo_product_tag_page_sidebar_position', 'no-sidebar' ) == 'no-sidebar' ) {
+} elseif ( function_exists( 'wc' ) && is_product_tag() && get_theme_mod( 'yith_proteo_product_tag_page_sidebar_position', 'no-sidebar' ) == 'no-sidebar' ) {
 	return;
-}
-
-// WooCommerce taxonomy page support.
-if ( function_exists( 'wc' ) && is_product_taxonomy() && get_theme_mod( 'yith_proteo_product_tax_page_sidebar_position', 'no-sidebar' ) == 'no-sidebar' ) {
+} elseif ( function_exists( 'wc' ) && is_product_taxonomy() && ! is_product_category() && get_theme_mod( 'yith_proteo_product_tax_page_sidebar_position', 'no-sidebar' ) == 'no-sidebar' ) {
 	return;
 }
 
@@ -35,6 +26,7 @@ if ( function_exists( 'wc' ) && is_product_taxonomy() && get_theme_mod( 'yith_pr
 
 <aside id="secondary" class="widget-area <?php echo '' != yith_proteo_get_sidebar_position() ? 'col-lg-3' : ''; ?>">
 	<?php
+
 	$sidebar = yith_proteo_sidebar_get_meta( 'sidebar_chooser' );
 	if ( class_exists( 'WooCommerce' ) ) {
 		if ( is_shop() ) {
