12975c367694ef68595d2d040eb6da3ca075090e
francgrasso <francgrasso@gmail.com>
2020-07-28 09:49:30 +0200

    new options for footer font size, color, link and alignment management. New options for footer credits font size, color, link and alignment management.

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -535,6 +535,97 @@ function yith_proteo_customize_register( $wp_customize ) {
 			)
 		)
 	);
+	// Footer font size options.
+	$wp_customize->add_setting(
+		'yith_proteo_footer_font_size',
+		array(
+			'sanitize_callback' => 'absint',
+			'default'           => 16,
+		)
+	);
+	$wp_customize->add_control(
+		'yith_proteo_footer_font_size',
+		array(
+			'label'   => esc_html__( 'Font size (default: 16px)', 'yith-proteo' ),
+			'section' => 'yith_proteo_footer_management',
+			'type'    => 'number',
+		)
+	);
+	// Footer font color options.
+	$wp_customize->add_setting(
+		'yith_proteo_footer_font_color',
+		array(
+			'sanitize_callback' => 'yith_proteo_sanitize_alpha_colors',
+			'default'           => '#ffffff',
+		)
+	);
+	$wp_customize->add_control(
+		new Customizer_Alpha_Color_Control(
+			$wp_customize,
+			'yith_proteo_footer_font_color',
+			array(
+				'label'   => esc_html__( 'Font color', 'yith-proteo' ),
+				'section' => 'yith_proteo_footer_management',
+			)
+		)
+	);
+	// Footer link color options.
+	$wp_customize->add_setting(
+		'yith_proteo_footer_link_color',
+		array(
+			'sanitize_callback' => 'yith_proteo_sanitize_alpha_colors',
+			'default'           => '#ffffff',
+		)
+	);
+	$wp_customize->add_control(
+		new Customizer_Alpha_Color_Control(
+			$wp_customize,
+			'yith_proteo_footer_link_color',
+			array(
+				'label'   => esc_html__( 'Hyperlink color', 'yith-proteo' ),
+				'section' => 'yith_proteo_footer_management',
+			)
+		)
+	);
+	// Footer link :hover color options.
+	$wp_customize->add_setting(
+		'yith_proteo_footer_link_hover_color',
+		array(
+			'sanitize_callback' => 'yith_proteo_sanitize_alpha_colors',
+			'default'           => yith_proteo_adjust_brightness( get_theme_mod( 'yith_proteo_footer_link_color', '#ffffff' ), - 0.3 ),
+		)
+	);
+	$wp_customize->add_control(
+		new Customizer_Alpha_Color_Control(
+			$wp_customize,
+			'yith_proteo_footer_link_hover_color',
+			array(
+				'label'   => esc_html__( 'Hyperlink :hover color', 'yith-proteo' ),
+				'section' => 'yith_proteo_footer_management',
+			)
+		)
+	);
+	// Footer alignment.
+	$wp_customize->add_setting(
+		'yith_proteo_footer_align',
+		array(
+			'default'           => 'right',
+			'sanitize_callback' => 'yith_proteo_sanitize_radio',
+		)
+	);
+	$wp_customize->add_control(
+		'yith_proteo_footer_align',
+		array(
+			'type'    => 'radio',
+			'label'   => esc_html__( 'Elements alignment', 'yith-proteo' ),
+			'section' => 'yith_proteo_footer_management',
+			'choices' => array(
+				'left'   => esc_html__( 'Left', 'yith-proteo' ),
+				'right'  => esc_html__( 'Right', 'yith-proteo' ),
+				'center' => esc_html__( 'Center', 'yith-proteo' ),
+			),
+		)
+	);
 
 	// Footer sidebar 1 options.
 	$wp_customize->add_setting(
@@ -613,7 +704,7 @@ function yith_proteo_customize_register( $wp_customize ) {
 			'type'    => 'textarea',
 		)
 	);
-	// Footer background color.
+	// Footer credit background color.
 	$wp_customize->add_setting(
 		'yith_proteo_footer_credits_background_color',
 		array(
@@ -629,6 +720,97 @@ function yith_proteo_customize_register( $wp_customize ) {
 				'label'   => esc_html__( 'Footer credits background color', 'yith-proteo' ),
 				'section' => 'yith_proteo_footer_credits_management',
 			)
+		)
+	);
+	// Footer credits font size options.
+	$wp_customize->add_setting(
+		'yith_proteo_footer_credits_font_size',
+		array(
+			'sanitize_callback' => 'absint',
+			'default'           => 16,
+		)
+	);
+	$wp_customize->add_control(
+		'yith_proteo_footer_credits_font_size',
+		array(
+			'label'   => esc_html__( 'Font size (default: 16px)', 'yith-proteo' ),
+			'section' => 'yith_proteo_footer_credits_management',
+			'type'    => 'number',
+		)
+	);
+	// Footer Credits font color options.
+	$wp_customize->add_setting(
+		'yith_proteo_footer_credits_font_color',
+		array(
+			'sanitize_callback' => 'yith_proteo_sanitize_alpha_colors',
+			'default'           => '#ffffff',
+		)
+	);
+	$wp_customize->add_control(
+		new Customizer_Alpha_Color_Control(
+			$wp_customize,
+			'yith_proteo_footer_credits_font_color',
+			array(
+				'label'   => esc_html__( 'Font color', 'yith-proteo' ),
+				'section' => 'yith_proteo_footer_credits_management',
+			)
+		)
+	);
+	// Footer Credits link color options.
+	$wp_customize->add_setting(
+		'yith_proteo_footer_credits_link_color',
+		array(
+			'sanitize_callback' => 'yith_proteo_sanitize_alpha_colors',
+			'default'           => '#ffffff',
+		)
+	);
+	$wp_customize->add_control(
+		new Customizer_Alpha_Color_Control(
+			$wp_customize,
+			'yith_proteo_footer_credits_link_color',
+			array(
+				'label'   => esc_html__( 'Hyperlink color', 'yith-proteo' ),
+				'section' => 'yith_proteo_footer_credits_management',
+			)
+		)
+	);
+	// Footer Credits link :hover color options.
+	$wp_customize->add_setting(
+		'yith_proteo_footer_credits_link_hover_color',
+		array(
+			'sanitize_callback' => 'yith_proteo_sanitize_alpha_colors',
+			'default'           => yith_proteo_adjust_brightness( get_theme_mod( 'yith_proteo_footer_credits_link_color', '#ffffff' ), - 0.3 ),
+		)
+	);
+	$wp_customize->add_control(
+		new Customizer_Alpha_Color_Control(
+			$wp_customize,
+			'yith_proteo_footer_credits_link_hover_color',
+			array(
+				'label'   => esc_html__( 'Hyperlink :hover color', 'yith-proteo' ),
+				'section' => 'yith_proteo_footer_credits_management',
+			)
+		)
+	);
+	// Footer Credits alignment.
+	$wp_customize->add_setting(
+		'yith_proteo_footer_credits_align',
+		array(
+			'default'           => 'right',
+			'sanitize_callback' => 'yith_proteo_sanitize_radio',
+		)
+	);
+	$wp_customize->add_control(
+		'yith_proteo_footer_credits_align',
+		array(
+			'type'    => 'radio',
+			'label'   => esc_html__( 'Elements alignment', 'yith-proteo' ),
+			'section' => 'yith_proteo_footer_credits_management',
+			'choices' => array(
+				'left'   => esc_html__( 'Left', 'yith-proteo' ),
+				'right'  => esc_html__( 'Right', 'yith-proteo' ),
+				'center' => esc_html__( 'Center', 'yith-proteo' ),
+			),
 		)
 	);
 
