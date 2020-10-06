0955b7d7d6fb74721c22d8afbbb8f0697e2e0a7a
francgrasso <francgrasso@gmail.com>
2020-10-06 12:46:54 +0200

    remove product short description from Products widget

--- woocommerce/content-widget-product.php
+++ woocommerce/content-widget-product.php
@@ -38,15 +38,12 @@ if ( ! is_a( $product, 'WC_Product' ) ) {
 			<span class="product-title"><?php echo wp_kses_post( $product->get_name() ); ?></span>
 		</a>
 
-
 		<?php if ( ! empty( $show_rating ) ) : ?>
 			<?php echo wc_get_rating_html( $product->get_average_rating() ); // PHPCS:Ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 		<?php endif; ?>
 
 		<?php echo $product->get_price_html(); // PHPCS:Ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-		<div class="widget-product-short-description">
-			<?php echo apply_filters( 'woocommerce_short_description', $product->get_short_description() ); // PHPCS:Ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-		</div>
+
 	</div>
 
 	<?php do_action( 'woocommerce_widget_product_item_end', $args ); ?>
